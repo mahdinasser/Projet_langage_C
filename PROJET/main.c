065f12f0ceeb1f395c6b8d8382606a9cff1a251a
mahdinasser <56402713+mahdinasser@users.noreply.github.com>
2020-01-20 17:17:05 +0100

    création de menu

---
+++ PROJET/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int Choice;
+    printf("MENU:\n------------------\n");
+    printf("1. Create new game\n");
+    printf("2. load saved game\n");
+    printf("3. About\n");
+    printf("4. Exit\n");
+    printf("Please enter your choice\n");
+    scanf("%d",&Choice);
+
+    switch(Choice)
+    {
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+    default:
+        break;
+    }
+
+    return 0;
+}
